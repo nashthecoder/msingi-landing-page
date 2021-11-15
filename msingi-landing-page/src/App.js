26612ec2983c903e3a34bc073a4e665b36c9a742
NashTheCoder <naijeria@gmail.com>
2021-11-16 02:04:57 +0300

    created steup files and pages

---
+++ msingi-landing-page/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Banner from "./components/Banner";
+import Form from "./components/Form";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <h1>Msingi Sass</h1>
+      <Banner/>
+      <Form/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
